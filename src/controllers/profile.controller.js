1c9d1c162effbe5dcf26b806dc45da4cf8458db2
shamsullah-shams <shamsullah07073@gmail.com>
2023-11-24 22:06:23 +0430

    add experience to freelancer

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -2,14 +2,145 @@ const httpStatus = require('http-status');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
-const { userService } = require('../services');
+const { freelancerService } = require('../services');
 
-const updateUser = catchAsync(async (req, res) => {
-  console.log(req.user);
-  const user = await userService.updateUserById(req.user._id, req.body);
-  res.send(user);
+const updateProfile = catchAsync(async (req, res) => {
+  const { user } = req;
+  if (user.role === 'freelancer') {
+    console.log(req.body);
+    const freelancer = await freelancerService.findFreelancerByUserId(user._id);
+    if (!freelancer) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Freelancer Not Found');
+    }
+
+    // update freelancer
+    const result = await freelancerService.updateFreelancerById(freelancer, req.body);
+    return res.status(httpStatus.ACCEPTED).send(result);
+  } else if (user.role === 'client') {
+  }
+
+  throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Role');
+});
+
+const addExperience = catchAsync(async (req, res) => {
+  const { user } = req;
+  if (user.role === 'freelancer') {
+    const freelancer = await freelancerService.findFreelancerByUserId(user._id);
+    if (!freelancer) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Freelancer Not Found');
+    }
+    // update freelancer
+    const result = await freelancerService.addExperience(freelancer._id, req.body);
+    return res.status(httpStatus.ACCEPTED).send(result);
+  } else if (user.role === 'client') {
+  }
+
+  throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Role');
+});
+
+const addEducation = catchAsync(async (req, res) => {
+  const { user } = req;
+  if (user.role === 'freelancer') {
+    const freelancer = await freelancerService.findFreelancerByUserId(user._id);
+    if (!freelancer) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Freelancer Not Found');
+    }
+    // update freelancer
+    const result = await freelancerService.addExperience(freelancer._id, req.body);
+    return res.status(httpStatus.ACCEPTED).send(result);
+  } else if (user.role === 'client') {
+  }
+
+  throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Role');
+});
+
+const addLanguages = catchAsync(async (req, res) => {
+  const { user } = req;
+  if (user.role === 'freelancer') {
+    const freelancer = await freelancerService.findFreelancerByUserId(user._id);
+    if (!freelancer) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Freelancer Not Found');
+    }
+    // update freelancer
+    const result = await freelancerService.addLanguages(freelancer._id, req.body);
+    return res.status(httpStatus.ACCEPTED).send(result);
+  } else if (user.role === 'client') {
+  }
+
+  throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Role');
+});
+
+const addSkills = catchAsync(async (req, res) => {
+  const { user } = req;
+  if (user.role === 'freelancer') {
+    const freelancer = await freelancerService.findFreelancerByUserId(user._id);
+    if (!freelancer) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Freelancer Not Found');
+    }
+    // update freelancer
+    const result = await freelancerService.addSkills(freelancer._id, req.body);
+    return res.status(httpStatus.ACCEPTED).send(result);
+  } else if (user.role === 'client') {
+  }
+
+  throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Role');
+});
+
+const addBiography = catchAsync(async (req, res) => {
+  const { user } = req;
+  if (user.role === 'freelancer') {
+    const freelancer = await freelancerService.findFreelancerByUserId(user._id);
+    if (!freelancer) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Freelancer Not Found');
+    }
+    // update freelancer
+    const result = await freelancerService.addBiography(freelancer._id, req.body.biography);
+    return res.status(httpStatus.ACCEPTED).send(result);
+  } else if (user.role === 'client') {
+  }
+
+  throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Role');
+});
+
+const addHourlyRate = catchAsync(async (req, res) => {
+  const { user } = req;
+  if (user.role === 'freelancer') {
+    const freelancer = await freelancerService.findFreelancerByUserId(user._id);
+    if (!freelancer) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Freelancer Not Found');
+    }
+    // update freelancer
+    const result = await freelancerService.addHourlyRate(freelancer._id, req.body.biography);
+    return res.status(httpStatus.ACCEPTED).send(result);
+  } else if (user.role === 'client') {
+  }
+
+  throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Role');
+});
+
+const addLocation = catchAsync(async (req, res) => {
+  const { user } = req;
+  if (user.role === 'freelancer') {
+    const freelancer = await freelancerService.findFreelancerByUserId(user._id);
+    if (!freelancer) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Freelancer Not Found');
+    }
+    // update freelancer
+    const result = await freelancerService.addLocation(freelancer._id, req.body.biography);
+    return res.status(httpStatus.ACCEPTED).send(result);
+  } else if (user.role === 'client') {
+  }
+
+  throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Role');
 });
 
 module.exports = {
-  updateUser,
+  addSkills,
+  addLocation,
+  addLanguages,
+  addEducation,
+  addBiography,
+  updateProfile,
+  addExperience,
+  addHourlyRate,
 };
